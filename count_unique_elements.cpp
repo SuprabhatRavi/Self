394ffefc1687c0d530b916b9b15fd16208aae504
SuprabhatRavi <62655280+SuprabhatRavi@users.noreply.github.com>
2021-07-27 11:19:14 +0530

    Update and rename unique_elements.cpp to count_unique_elements.cpp

--- count_unique_elements.cpp
+++ count_unique_elements.cpp
@@ -17,3 +17,19 @@ int main() {
 	
 	return 0;
 }
+
+//better code
+
+#include <iostream>
+#include<unordered_set>
+using namespace std;
+
+int main() {
+	int arr[]= {1, 2, 3, 3, 4, 5, 6, 6, 7};
+	int n= sizeof(arr)/sizeof(arr[0]);
+	
+	unordered_set<int> s(arr, arr+n);
+
+	cout<<s.size();
+	return 0;
+}
