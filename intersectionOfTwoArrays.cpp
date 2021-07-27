b8322f6c3cdab60aedebb74fe19de685846b4c57
SuprabhatRavi <62655280+SuprabhatRavi@users.noreply.github.com>
2021-07-27 14:19:10 +0530

    Update intersectionOfTwoArrays.cpp

--- intersectionOfTwoArrays.cpp
+++ intersectionOfTwoArrays.cpp
@@ -1,3 +1,5 @@
+//intersection of 2 sorted arrays
+
 #include <iostream>
 using namespace std;
 
@@ -19,6 +21,31 @@ int main() {
 	    }
 	    else if(arr[i]<arr1[j])i++;
 	    else j++;
+	}
+	return 0;
+}
+
+//Using unordered_set
+
+#include <iostream>
+#include<unordered_set>
+using namespace std;
+
+int main() {
+	int arr[]={1,1,3,3,4,6,10};
+	int arr1[]={1,5,5,5,6,10,11};
+	int n=sizeof(arr)/sizeof(arr[0]), m=sizeof(arr1)/sizeof(arr1[0]);
+	unordered_set<int> s;
+	
+	for(int i=0; i<n; i++){
+	    s.insert(arr[i]);
+	}
+	
+	for(int i=0; i<m; i++){
+	    if(s.find(arr1[i])!=s.end()){
+	        cout<<arr1[i]<<" ";
+	        s.erase(arr1[i]);
+	    }
 	}
 	return 0;
 }
